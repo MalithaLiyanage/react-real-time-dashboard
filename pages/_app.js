124d01d16df8c7af17e88186fd372815773f3572
MalithaLiyanage <malitharanu82@gmail.com>
2022-08-13 18:20:31 +0530

    Restructuring the project code

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/app.scss';
 import { MantineProvider } from '@mantine/core';
 import { rtlCache } from '../rtl-cache';
+import {DefaultLayout} from '../components/layouts';
 
 function MyApp({ Component, pageProps }) {
   return <MantineProvider
@@ -11,7 +12,9 @@ function MyApp({ Component, pageProps }) {
         }}
         emotionCache={rtlCache}
       >
-        <Component {...pageProps} />
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
       </MantineProvider>
 }
 
